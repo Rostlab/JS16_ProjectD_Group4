0f8212a75f05280cd0f511a82baea3bc2c14b322
Julien Schmidt <git@julienschmidt.com>
2016-03-05 23:08:48 +0100

    crawler: get characters

--- crawler.js
+++ crawler.js
@@ -1,7 +1,14 @@
 // TODO: merge this into app.js
 "use strict";
 
-const twitter = require('./crawler/twitter');
-      //got     = require('./crawler/got');
+const twitter = require('./crawler/twitter'),
+      got     = require('./crawler/got');
 
-twitter.fetchTweets("tyrion lannister", null, 10);
+
+//twitter.fetchTweets("tyrion lannister", null, 50);
+got.fetchCharacters(function(status, characters) {
+    console.log(status);
+    console.log(characters);
+}, function(err) {
+    console.log(err);
+});
