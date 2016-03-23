70a0b0677e55c0b59371ecf52d3d60aaea4b9af0
Julien Schmidt <git@julienschmidt.com>
2016-03-23 23:32:09 +0100

    pkg: wrap update in Promise

--- index.js
+++ index.js
@@ -49,20 +49,20 @@ pkg.shutdown = function() {
  * @param {boolean} [full=false]  full rebuild or incremental update
  */
 pkg.update = function(full) {
-    got.updateCharacters().then(function(res) {
-        debug.info("Characters updated", res);
+    return new Promise(function(resolve, reject) {
+        got.updateCharacters().then(function(res) {
+            debug.info("Characters updated", res);
 
-        mobile.crawlAll(full).then(function(res) {
-            debug.info("MCRAWL FINISHED: ", res);
-        }, debug.error);
+            mobile.crawlAll(full).then(function(res) {
+                resolve(res);
+            }, reject);
 
-        // twitter.crawlAll().then(function(res) {
-        //     debug.info("ACRAWL FINISHED: ", res);
-        // }).catch(debug.eror);
+            // twitter.crawlAll().then(function(res) {
+            //     debug.info("ACRAWL FINISHED: ", res);
+            // }).catch(debug.eror);
 
-        // TODO: Analyze all
-    }, function(err) {
-        debug.error("Updating Characters: ", err);
+            // TODO: Analyze all
+        }, reject);
     });
 };
 
