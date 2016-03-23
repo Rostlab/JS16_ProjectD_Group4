3d993218afb3d62f72ee93e460704eee328db7a7
Julien Schmidt <git@julienschmidt.com>
2016-03-23 01:25:05 +0100

    core/config: cfg.extend

--- core/config.js
+++ core/config.js
@@ -1,5 +1,7 @@
+"use strict";
+
 const fs    = require('fs'),
-      debug = require('../core/debug');
+      debug = require('../core/debug')('core/config', true);
 
 // read config
 var cfg;
@@ -7,11 +9,57 @@ try {
     cfg = JSON.parse(fs.readFileSync('defaults.json', 'utf8'));
 } catch (e) {
     if (e.code === 'ENOENT') {
-        debug.log('Error: defaults.json not found!');
+        debug.error('defaults.json not found!');
     } else {
-        debug.log('Error:', e);
+        debug.error(e);
     }
     process.exit(1);
 }
+
+// removes duplicate items from given array (nice, eh?)
+function deduplicate(a) {
+    var res = [];
+    var key = {};
+    a.map(function(v) {
+        key[v] = key[v] || res.push(v);
+    });
+    return res;
+}
+
+// extend base object with ext object
+// leaf attribute are overwritten, arrays appended (duplicate free).
+// no new properties are added
+function extend(base, ext) {
+    for (var attr in base) {
+        if (ext.hasOwnProperty(attr)) {
+            // Objects and Arrays
+            if (typeof base[attr] === 'object') {
+                if (Array.isArray(base[attr])) {
+                    if (!Array.isArray(ext[attr])) {
+                        throw 'Value for ' + attr + ' not an Array: ' + ext;
+                    }
+                    base[attr] = deduplicate(base[attr].concat(ext[attr]));
+                } else {
+                    if (typeof ext[attr] !== 'object') {
+                        throw 'Value for ' + attr + ' not an Object: ' + ext;
+                    }
+                    extend(base[attr], ext[attr]);
+                }
+
+            // Value
+            } else {
+                base[attr] = ext[attr];
+            }
+        }
+    }
+}
+
+// let user overwrite cfg defaults
+cfg.extend = function(json) {
+    if (typeof json !== 'object') {
+        throw 'Not JSON: ' + json;
+    }
+    extend(cfg, json);
+};
 
 module.exports = cfg;
