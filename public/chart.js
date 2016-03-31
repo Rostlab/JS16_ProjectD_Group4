81b1195e9c51ec4dfe54683cf4318bcfdf3b06da
marcusnovotny <marcusnovotny@gmx.de>
2016-03-31 02:31:04 +0200

    improves tool design & code layout

--- public/chart.js
+++ public/chart.js
@@ -1,31 +1,36 @@
 // function to parse string in the format 2006-01-02 to JavaScript's Date type
 var parseDate = d3.time.format("%Y-%m-%d").parse;
 
-function characterChart(svg, dataURL) {
+// returns the characterChart
+function characterChart(svg, dataURL, startDate, endDate) {
     var self = this;
     var zoom = d3.behavior.zoom(); // Zooming behavior
     this.fullYdomain = [];
-    this.fullXdomain = [];
+    this.fullXdomain = []; // Range of available data
+    this.xDomainBounds = []; // Range of shown data
     this.resize = render; // Resizing behaviour
     this.episodeData = null;
     this.ready = 0;
+    // startDate & endDate are optional parameters. Default values will be overwritten later.
+    this.startDate = startDate;
+    this.endDate = endDate;
 
-    // Get CSV prefix (HELPER Function)
+    // Get CSV prefix
     function getPrefix() {
         var path = dataURL.split("/");
         path.pop();
         var s = "";
-        for (var p of path) {
-            s += p + "/";
+        for (var i = 0; i < path.length; i++) {
+            s += path[i] + "/";
         }
         return s;
     }
 
     // set margin
     var margin = {
-        top: 40,
+        top: 30,
         right: 20,
-        bottom: 50,
+        bottom: 30,
         left: 50
     };
 
@@ -46,25 +51,21 @@ function characterChart(svg, dataURL) {
         y = d3.scale.linear()
         .range([getSize().height, 0]);
 
-    var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-        yAxis = d3.svg.axis().scale(y).orient("left");
+    var xAxis = d3.svg.axis().scale(x).orient("bottom")
+        .outerTickSize(0),
+        yAxis = d3.svg.axis().scale(y).orient("left")
+        .outerTickSize(0);
+
+    // Since there is only width-resizing: Define the number of yAxis-ticks according to screen size.
+    // Default value is 10. Suitable for most cases.
+    if (getSize().height < 200) {
+        yAxis.ticks(6);
+    } else if (getSize().height < 300) {
+        yAxis.ticks(8);
+    }
 
     // Seperate axis for episodes
     var eAxis = d3.svg.axis().scale(x).orient("top").tickSize(0, 0);
-
-    // TODO: Improve Multi-Scale Tick Format on time x-axis
-    // Cheat sheet: https://github.com/mbostock/d3/wiki/Time-Formatting#format_iso
-    /*    var customTimeFormat = d3.time.format.multi([
-            [".%L", function(d) { return d.getMilliseconds(); }],
-            [":%S", function(d) { return d.getSeconds(); }],
-            ["%I:%M", function(d) { return d.getMinutes(); }],
-            ["%I %p", function(d) { return d.getHours(); }],
-            ["%a %d", function(d) { return d.getDay() && d.getDate() != 1; }],
-            ["%b %d", function(d) { return d.getDate() != 1; }],
-            ["%B", function(d) { return d.getMonth(); }],
-            ["%Y", function() { return true; }]
-        ]);
-        xAxis.tickFormat(customTimeFormat);*/
 
     // positive area between x-axis at y=0 and max(y)
     var calcAreaPos = d3.svg.area()
@@ -76,9 +77,6 @@ function characterChart(svg, dataURL) {
         }).y1(function (d) {
             return y(d.pos);
         });
-    /*        .defined(function (d) {
-                return d.pos > 0;
-            });*/
 
     // negative area between x-axis at y=0 and min(y)
     var calcAreaNeg = d3.svg.area()
@@ -90,9 +88,17 @@ function characterChart(svg, dataURL) {
         }).y1(function (d) {
             return y(0);
         });
-    /*        .defined(function (d) {
-                return d.pos > 0;
-            });*/
+
+    // trendline
+    var calcTrend = d3.svg.line()
+        .interpolate("cardinal-open")
+        .tension(0)
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d.pos + d.neg);
+        });
 
     // Outer container for the timeline
     var container = svg.append("g")
@@ -101,20 +107,22 @@ function characterChart(svg, dataURL) {
         .attr("transform", "translate(" + (margin.left) + "," + margin.top + ")");
 
     // Create a background area
-    var plot = svg.append("g");
+    var plot = container.append("g");
     var background = plot.append("rect")
-        .attr("id", "background")
-        .attr("class", "react")
-        .attr("width", getSize().width - 1)
-        .attr("height", getSize().height - 1)
-        .attr("x", "1")
-        .attr("y", "1")
-        .style("fill", "#EEEEEE");
-    // Moving to the right by 1px to prevent shadowing of y axis.
+        .attr("class", "react background")
+        .attr("width", getSize().width)
+        .attr("height", getSize().height);
+
+    // Create area-dividing line
+    var y0line = plot.append("line")
+        .attr("x1", "0")
+        .attr("x2", getSize().width)
+        .style("stroke", "#000");
 
     // create group for chart within svg
     var chart = plot.append("g")
-        .attr("class", "react");
+        .attr("class", "react")
+        .attr("clip-path", "url(#clip)");
 
     // Define the visible area for the plot & eLabel (needs clip path because of customized tick values)
     var clipper = container.append("defs").append("clipPath")
@@ -125,18 +133,24 @@ function characterChart(svg, dataURL) {
         .attr("width", background.attr("width"))
         .attr("height", background.attr("height") + margin.top);
 
-    // apply margin
-    plot.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
     // add area elements
     var pos = chart.append("path").attr("class", "area pos"),
         neg = chart.append("path").attr("class", "area neg");
 
+    // add trendline
+    var trendline = chart.append("path").attr("class", "trendline");
+
     // add axis label elements
     var xLabel = container.append("g").attr("class", "x axis")
-        .attr("transform", "translate(0," + getSize().height + ")"),
+        .attr("transform", "translate(0," + (getSize().height) + ")"),
         yLabel = container.append("g").attr("class", "y axis");
     var eLabel = container.append("g").attr("class", "x axis").attr("clip-path", "url(#clip)");
+
+    // add right border
+    var rightBorder = container.append("rect")
+        .attr("x", getSize().width)
+        .attr("height", getSize().height)
+        .style("fill", "#000");
 
     function convertTwitterCSV(d) {
         // convert string data from CSV
@@ -163,7 +177,7 @@ function characterChart(svg, dataURL) {
         };
     }
 
-    // CURRENTLY BEING DEVELOPED: 
+    // Fills in missing csv data
     function assignDefaultValues(error, data) {
         var newData = [];
         var dateRange = d3.extent(data, function (d) {
@@ -174,8 +188,6 @@ function characterChart(svg, dataURL) {
         };
         var stepdate = new Date(dateRange[0]);
 
-        data.sort(sortByDate);
-
         for (var i = 0; i < data.length;) {
             if ((data[i].date - stepdate) === 0) {
                 stepdate.setDate(stepdate.getDate() + 1);
@@ -185,7 +197,8 @@ function characterChart(svg, dataURL) {
                     newData.push({
                         date: new Date(stepdate), // Screw closures!
                         pos: 0,
-                        neg: 0
+                        neg: 0,
+                        avg: 0
                     });
                     stepdate.setDate(stepdate.getDate() + 1);
                 }
@@ -202,18 +215,26 @@ function characterChart(svg, dataURL) {
             throw up;
         }
 
-        // set domains (data)
+        // fullXdomain: Range of available data
         self.fullXdomain = d3.extent(data, function (d) {
             return d.date;
         });
 
-        // Show all the available data.         
-        x.domain(self.fullXdomain);
+        // xDomainBounds: Max range of scrollable graph area. At least slightly before the first season, up until today.
+        // We should extend the graph all the way back even if there's no data, to show that Twitter didn't care back then.
+        self.xDomainBounds = [new Date(2010, 8, 1), new Date()];
+        /*      if (self.fullXdomain[0] < self.xDomainBounds[0]) {
+                  self.xDomainBounds[0] = self.fullXdomain[0];
+              };*/
 
-        // Show default date range. Could for example be set to the current week. Currently not in use.
-        /*        var minDate = new Date(2016, 1, 20),
-                    maxDate = new Date(2016, 2, 5);
-                x.domain([minDate, maxDate]);*/
+        // Set initial domain. If not defined during the call: All the available data.
+        if (typeof self.startDate === "undefined") {
+            self.startDate = new Date(2014, 0, 1);
+        }
+        if (typeof self.endDate === "undefined") {
+            self.endDate = self.xDomainBounds[1];
+        }
+        x.domain([self.startDate, self.endDate]);
 
         // y from min(neg) to max(pos)
         y.domain([d3.min(data, function (d) {
@@ -227,14 +248,14 @@ function characterChart(svg, dataURL) {
         // set area data
         pos.datum(data);
         neg.datum(data);
+        trendline.datum(data);
 
         // Zoom Handling
         var currentDomain = x.domain()[1] - x.domain()[0],
-            minScale = currentDomain / (self.fullXdomain[1] - self.fullXdomain[0]), // All the available data
+            minScale = currentDomain / (self.xDomainBounds[1] - self.xDomainBounds[0]), // All the available data
             maxScale = currentDomain / (1000 * 60 * 60); // 1 hour
 
         zoom.x(x)
-            // .xExtent(fullXdomain) Unfortunately not implemented in current d3js version :(
             .scaleExtent([minScale, maxScale])
             .on("zoom", zoomed);
         go();
@@ -254,54 +275,53 @@ function characterChart(svg, dataURL) {
             .enter().append("rect")
             .attr("class", "episode react")
             .attr("clip-path", "url(#clip)")
-            .attr("x", function (d) {
-                return x(d.date);
-            })
             .attr("y", 0)
-            .attr("width", 1)
             .attr("height", getSize().height);
 
         eAxis.tickValues(function () {
             var a = [];
-            for (var d of data) {
-                a.push(d.date);
+            for (var i = 0; i < data.length; i++) {
+                a.push(data[i].date);
             }
             return a;
         });
 
-        eLabel.call(eAxis); // Has to be called once initially       
-
         go();
-    }
-
-    function render() {
-        // axis (not needed for zoom handling)
-        var s = getSize();
-        x.range([0, s.width]);
-        y.range([s.height, 0]);
-        background.attr("width", s.width - 1);
-        clipper.attr("width", s.width - 1);
-        plot.attr("width", s.width - 1);
-
-        recalc();
     }
 
     function zoomed() {
         // Workaround because .xExtent() for the zoom behaviour is not part of the current official d3js release
-        if (x.domain()[0] < self.fullXdomain[0]) {
-            zoom.translate([zoom.translate()[0] - x(self.fullXdomain[0]) + x.range()[0], 0]);
-        } else if (x.domain()[1] > self.fullXdomain[1]) {
-            zoom.translate([zoom.translate()[0] - x(self.fullXdomain[1]) + x.range()[1], 0]);
+        if (x.domain()[0] < self.xDomainBounds[0]) {
+            zoom.translate([zoom.translate()[0] - x(self.xDomainBounds[0]) + x.range()[0], 0]);
+        } else if (x.domain()[1] > self.xDomainBounds[1]) {
+            zoom.translate([zoom.translate()[0] - x(self.xDomainBounds[1]) + x.range()[1], 0]);
         }
         recalc();
     }
 
+    // Operations which are needed for drawing & resizing the graph (not for zooming)
+    function render() {
+        var s = getSize();
+        x.range([0, s.width]);
+        y.range([s.height, 0]);
+        background.attr("width", s.width);
+        clipper.attr("width", s.width);
+        plot.attr("width", s.width);
+        rightBorder.attr("x", s.width);
+        y0line.attr("x2", s.width)
+            .attr("y1", y(0))
+            .attr("y2", y(0));
+
+        recalc();
+    }
+
+    // Operations which are needed for drawing, resizing & zooming
     function recalc() {
-        // areas
         pos.attr("d", calcAreaPos);
         neg.attr("d", calcAreaNeg);
+        trendline.attr("d", calcTrend);
 
-        updateEpisodeLabels();
+        updateLabels();
 
         // axis
         xLabel.call(xAxis);
@@ -309,14 +329,14 @@ function characterChart(svg, dataURL) {
         eLabel.call(eAxis);
     }
 
-    function updateEpisodeLabels() {
+    // Updates episode labels & reacts to screen size
+    function updateLabels() {
         var dmn = x.domain()[1] - x.domain()[0]; // Current domain
         var w = getSize().width;
-
-        // episode rectangles
         var dayWidth = (86400000 / dmn) * w;
-        var szn =
-            d3.selectAll(".episode")
+
+        // episode rectangles        
+        var szn = d3.selectAll(".episode")
             .attr("x", function (d) {
                 return x(d.date);
             })
@@ -325,12 +345,14 @@ function characterChart(svg, dataURL) {
             });
 
         // Cheap Screen Size factor
-        var factor = 1;
+        var factor = 1; // BETTER: 2/5, 3/5, 4/5, 1 (0?)
         if (w < 900) {
             factor = 1 / 3;
         } else if (w < 1400) {
             factor = 2 / 3;
         }
+
+        xAxis.ticks(10 * factor);
 
         // Move the Labels into the center of the day 
         eLabel.selectAll('.tick text')
@@ -345,11 +367,7 @@ function characterChart(svg, dataURL) {
             eAxis.tickFormat(function (d, i) {
                 return self.episodeData[i].code;
             });
-        } else if (dmn < (10 * 365 * 86400000)) { // < 10 Years * factor: Show season start
-            /* Move the Labels into the center of the season (avg a little over 2 months)
-            Won't work with the newest season during the time it's airing. Therefore commented out for now.
-            eLabel.selectAll('.tick text')
-                .attr('transform', 'translate(' + (dayWidth*64) / 2 + ',0)');*/
+        } else if (w > 500) { //Show season start
             eAxis.tickFormat(function (d, i) {
                 return self.episodeData[i].seasonStartLabel;
             });
@@ -362,13 +380,17 @@ function characterChart(svg, dataURL) {
     d3.csv(dataURL, convertTwitterCSV, assignDefaultValues);
 
     // request csv episodes data and create episode labels
-    d3.csv("" + getPrefix() + "episodes.csv", convertEpisodesCSV, handleEpisodes);
+    d3.csv(getPrefix() + "episodes.csv", convertEpisodesCSV, handleEpisodes);
 
     // Initial rendering when fillChart & handleEpisodes have finished (ready = 2)        
     function go() {
         self.ready++;
         if (self.ready === 2) {
             render();
+
+            // Finally draw static graph elements. Looks weird if they are drawn right from the beginning
+            rightBorder.attr("width", 1);
+            y0line.attr("stroke-width", 0.5);
 
             // Add zoom handlers
             d3.selectAll(".react").call(zoom);
