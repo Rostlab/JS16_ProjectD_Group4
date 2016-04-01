c6255fc0cbf4ac3fed0b667b161d3a92462d0ee1
Julien Schmidt <git@julienschmidt.com>
2016-04-01 23:50:59 +0200

    aggregator/sentiment: add values from cfg

--- aggregator/sentiment.js
+++ aggregator/sentiment.js
@@ -1,9 +1,13 @@
-const polarity = require('polarity');
+"use strict";
+
+const cfg      = require('../core/config'),
+      polarity = require('polarity');
 
 function tokenize(value) {
     return value.toLowerCase().match(/\S+/g);
 }
 
 module.exports = function(text) {
+    polarity.inject(cfg.sentimentlist);
     return polarity(tokenize(text)).polarity;
 };
